87f711b279f7f493f587ebf889d2ee28cbc81133
maustem <diego.maustem@gmail.com>
2024-09-20 14:17:18 -0300

    :test_tube: test: Implementation functions typescript

---
+++ functions.js
@@ -0,0 +1,5 @@
+var sum = function (x, y) {
+    return (x + y);
+};
+var result = sum(2, 3);
+console.log(result);
---
+++ functions.ts
@@ -0,0 +1,7 @@
+const sum = (x: number, y: number): number | string => {
+    return (x + y)
+}
+
+const result = sum(2, 3)
+
+console.log(result)
